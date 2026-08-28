d7ae1336925cf7362273a8037b8046dfecc3f882
SabariGirish2811 <ssabarigirishknuckles@gmail.com>
2026-08-28 12:11:41 +0530

    Fix bugs on international players and role discrepancies

--- src/data/cleandata.js
+++ src/data/cleandata.js
@@ -1,16 +1,10 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import fs from 'fs';
 
-// Resolve current directory in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Read and parse the JSON file safely using fs
-const rawData = fs.readFileSync(path.join(__dirname, 'players.json'), 'utf-8');
+// Read the JSON file natively using fs and import.meta.url to bypass 'require'
+const rawData = fs.readFileSync(new URL('./players.json', import.meta.url), 'utf8');
 const players = JSON.parse(rawData);
 
-// 1. Curated Set of Authentic Wicketkeepers
+// 1. Authentic Wicketkeepers
 const validWKs = new Set([
   "MS Dhoni", "Dinesh Karthik", "Rishabh Pant", "Sanju Samson", "Ishan Kishan", 
   "Quinton de Kock", "Jos Buttler", "KL Rahul", "N Pooran", "Jonny Bairstow", 
@@ -23,7 +17,7 @@ const validWKs = new Set([
   "Brendon McCullum", "T Kohler-Cadmore", "Josh Inglis", "Ryan Rickelton"
 ]);
 
-// 2. Comprehensive List of IPL Overseas Players
+// 2. Updated Overseas Players (including the missing ones you requested)
 const overseasPlayers = new Set([
   "David Warner", "Tymal Mills", "Moises Henriques", "Shane Watson", "Travis Head", "Ben Cutting",
   "Chris Gayle", "Rashid Khan", "Jos Buttler", "Ben Stokes", "Imran Tahir", "Adam Zampa",
@@ -34,22 +28,24 @@ const overseasPlayers = new Set([
   "Lendl Simmons", "Matt Henry", "Isuru Udana", "Jason Holder", "Nathan Coulter-Nile", "D'Arcy Short",
   "Ben Laughlin", "Tom Curran", "Colin Munro", "Mujeeb Ur Rahman", "Akila Dananjaya", "Oshane Thomas",
   "Hardus Viljoen", "Dushmantha Chameera", "Matthew Wade", "Romario Shepherd", "Fazalhaq Farooqi",
-  "Harry Brook", "Glenn Phillips", "RJW Topley", "Cameron Green", "Michael Bracewell", "Matheesha Pathirana",
-  "Sisanda Magala", "Dwaine Pretorius", "Tristan Stubbs", "Matthew Short", "WD Parnell", "Lungi Ngidi",
-  "Akeal Hosein", "Dasun Shanaka", "Kyle Jamieson", "Josh Hazlewood", "Fabian Allen", "Blessing Muzarabani",
+  "Harry Brook", "Glenn Phillips", "Reece Topley", "Cameron Green", "Michael Bracewell", "Matheesha Pathirana",
+  "Sisanda Magala", "Dwaine Pretorius", "Tristan Stubbs", "Matthew Short", "Wayne Parnell", "Lungi Ngidi",
+  "Akeal Hosein", "Dasun Shanaka", "Kyle Jamieson", "Josh Hazlewood", "Finn Allen", "Fabian Allen", "Blessing Muzarabani",
   "Ricky Ponting", "Jacques Kallis", "David Hussey", "Cameron White", "Mohammad Hafeez", "Ashley Noffke",
   "Matthew Hayden", "Simon Katich", "Glenn McGrath", "B Geeves", "Darren Lehmann", "Shane Warne",
   "Luke Ronchi", "Sanath Jayasuriya", "Dominic Thornely", "Shaun Pollock", "Evin Lewis", "Faf du Plessis",
   "Kane Williamson", "Colin de Grandhomme", "Quinton de Kock", "Andre Russell", "Lockie Ferguson",
-  "Rilee Rossouw", "Jake Fraser-McGurk", "Shai Hope", "Shamar Joseph", "WG Jacks", "Liam Livingstone",
+  "Rilee Rossouw", "Jake Fraser-McGurk", "Shai Hope", "Shamar Joseph", "Will Jacks", "Liam Livingstone",
   "Jhye Richardson", "Riley Meredith", "Daniel Vettori", "Marlon Samuels", "Corey Anderson",
-  "Jason Behrendorff", "Dawid Malan", "Kyle Mayers", "Noor Ahmad", "Dilshan Madushanka", "KMA Paul",
+  "Jason Behrendorff", "Dawid Malan", "Kyle Mayers", "Noor Ahmad", "Dilshan Madushanka", "Kemo Paul",
   "Junior Dala", "Javon Searles", "Colin Ingram", "Nandre Burger", "Gerald Coetzee", "Spencer Johnson",
   "Luke Wood", "Allah Ghazanfar", "David Wiese", "Jamie Overton", "George Linde", "David Payne", 
-  "Chris Green", "LB Williams", "SC Kuggeleijn", "Duan Jansen", "XC Bartlett", "M Jansen", "Andrew Tye", 
-  "M Theekshana", "PWA Mulder", "Eoin Morgan", "Alex Hales", "Ish Sodhi", "JDP Oram", "Muttiah Muralitharan", 
+  "Chris Green", "Lizaad Williams", "Scott Kuggeleijn", "Duan Jansen", "Xavier Bartlett", "Marco Jansen", "Andrew Tye", 
+  "Maheesh Theekshana", "Wiaan Mulder", "Eoin Morgan", "Alex Hales", "Ish Sodhi", "Jacob Oram", "Muttiah Muralitharan", 
   "Alzarri Joseph", "Josh Little", "N Thushara", "Cooper Connolly", "Matthew Breetzke", "Corbin Bosch", 
-  "Jacob Bethell", "Karim Janat", "Eshan Malinga", "William O'Rourke", "Gulbadin Naib", "Sediqullah Atal", "PVSN Raju"
+  "Jacob Bethell", "Karim Janat", "Eshan Malinga", "William O'Rourke", "Gulbadin Naib", "Sediqullah Atal", 
+  "Jean Paul Duminy", "Shimron Hetmyer", "Shimron Hetmyer", "Sandeep Lamichhane", 
+  "Mohammad Ashraful", "Mashrafe Mortaza"
 ]);
 
 const cleanedData = players.map(p => {
@@ -60,17 +56,13 @@ const cleanedData = players.map(p => {
     fixedRole = "Batter";
   }
 
-  // Rule 2: Flag Overseas Nationality
-  const isOverseas = overseasPlayers.has(p.name);
-
   return {
     ...p,
     role: fixedRole,
-    is_overseas: isOverseas
+    is_overseas: overseasPlayers.has(p.name)
   };
 });
 
-// Output the fixed file
-const outputPath = path.join(__dirname, 'players_fixed.json');
-fs.writeFileSync(outputPath, JSON.stringify(cleanedData, null, 2));
-console.log(`Successfully cleaned ${cleanedData.length} players and saved to players_fixed.json!`);
+// Write to a new JSON file
+fs.writeFileSync(new URL('./players_fixed.json', import.meta.url), JSON.stringify(cleanedData, null, 2));
+console.log(`Successfully cleaned ${cleanedData.length} players!`);
